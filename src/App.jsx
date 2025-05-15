135f66c6a09177b553b8f5dffa25d7edf336cb17
DipeshOmre <dipesh@gmail.com>
2025-05-15 10:18:12 +0530

    here we add routes to our application

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './App.css'
 const App = () => {
   return (
-    <div className='bg-red-500 text-white p-4'>
-      hi iam a tailwind css
-    </div>
+    <>
+    
+    </>
   )
 }
 
