ab691ae4301a49c973084e7fe02dbf3d20932489
DipeshOmre <dipesh@gmail.com>
2025-05-16 08:41:30 +0530

    Progress bar has been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import LoadingScreen from './components/LoadingScreen'
 const App = () => {
+//   const [isloadin, setIsloadin] = useState(false);
+// useEffect(() => {
+//   setTimeout(() => {
+//     setIsloadin(true);
+//   }, 2000);
+// },[])
+
+
   return (
     <>
-    
+    <LoadingScreen/>
+    <div>
+    hi there : 
+    </div>
     </>
   )
 }
