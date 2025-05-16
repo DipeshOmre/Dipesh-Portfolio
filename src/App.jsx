2f7cd163f97fcf6bac6b4da00e3b2d928f9de2cb
DipeshOmre <dipesh@gmail.com>
2025-05-16 22:37:31 +0530

    here we created our Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import LoadingScreen from './components/LoadingScreen'
+import Navbar from './components/Navbar';
 const App = () => {
   const [isloading, setIsloading] = useState(false);
 
@@ -9,8 +10,8 @@ const App = () => {
     <>
     {!isloading && <LoadingScreen onComplete={()=>setIsloading(true)}/>}
     {/* <LoadingScreen onComplete={()=>setIsloading(true)}/> */}
-    <div>
-    hi there : 
+    <div className={`min-h-screen transition-opacity duration-700 ${isloading ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
+      <Navbar/>
     </div>
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg shadow-lg border-b border-white/10'>
+
+        <div className='max-w-5xl mx-auto px-4'>
+            <div className='flex items-center justify-between h-16'>
+                <a href="#home" className='font-mono text-2xl font-bold text-white'>
+                    Dipp<span className='text-blue-500'>.tech</span>
+                </a>
+                <div className='w-7 h-5 relative cursor-pointer z-40 md:hidden'>
+                    &#9776;
+                </div>
+                <div className='hidden md:flex items-center space-x-8'>
+                <a href="#home" className='hover:text-white text-gray-300'>Home</a>
+                <a href="#about" className='hover:text-white text-gray-300'>About</a>
+                <a href="#projects" className='hover:text-white text-gray-300'>Projects</a>
+                <a href="#contact" className='hover:text-white text-gray-300'>Contact</a>
+                </div>
+            </div>
+            </div>        
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
